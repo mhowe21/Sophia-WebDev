3f5abb7cbebefc106a10a1f3f9ef5c2a3f27bf13
Mason Howe <mason.a.howe@gmail.com>
2024-06-23 12:04:32 -0600

    Add Cart and Contact pages

--- JS/PrimaryScript.js
+++ JS/PrimaryScript.js
@@ -1,5 +1,8 @@
-var button = document.getElementById("subscribeButton");
+const subscribeButton = document.getElementById("subscribeButton");
 
-button.addEventListener("click", function () {
+subscribeButton.addEventListener("click", function () {
+  let emailValue = document.getElementById("emailEntry").value;
+  sessionStorage.setItem("email", emailValue);
+  console.log(`${emailValue} was stored to session storage.`);
   alert("Thank you for subscribing!");
 });
